86b34e2f4aaabbb8fd5f9e86bf392c3b8a698722
Anatoliy Yanuzov <anatolii.qnuzov@gmail.com>
2022-03-26 13:08:16 +0200

    added a second solution

--- 06.Dynamic-Validation/app.js
+++ 06.Dynamic-Validation/app.js
@@ -1,21 +1,35 @@
 function validate() {
 
+    let emailValidatorRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g
     let emailInputElement = document.querySelector('#email');
-    let emailValidator = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g
 
-    function isValidEmail(str) {
-        if (emailValidator.test(str)) {
-            return true;
-        } else {
-            return false;
+    emailInputElement.addEventListener('change', checkEmail);
+
+    function checkEmail(e) {
+        if (emailValidatorRegex.test(e.target.value)) {
+            e.target.removeAttribute('class');
+            return;
         }
+        e.target.className = 'error';
     }
-    
-    function applyStyle(e, email) {
-        e.className = isValidEmail(email) ? '' : 'error';
-    }
-
-    emailInputElement.addEventListener('change', (e) => {
-        applyStyle(e.target, e.target.value)
-    });
 }
+// function validate() {
+
+//     let emailInputElement = document.querySelector('#email');
+//     let emailValidatorRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g
+    // function isValidEmail(str) {
+    //     if (emailValidatorRegex.test(str)) {
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+    // }
+
+    // function applyStyle(e, email) {
+    //     e.className = isValidEmail(email) ? '' : 'error';
+    // }
+
+    // emailInputElement.addEventListener('change', (e) => {
+    //     applyStyle(e.target, e.target.value)
+    // });
+// }

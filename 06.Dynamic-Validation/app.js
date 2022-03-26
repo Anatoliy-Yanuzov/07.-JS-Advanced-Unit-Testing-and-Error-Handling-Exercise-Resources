8f7aa0aefec0f365713aa849bfe14cea5467636c
Anatoliy Yanuzov <anatolii.qnuzov@gmail.com>
2022-03-26 02:11:25 +0200

    added solution

--- 06.Dynamic-Validation/app.js
+++ 06.Dynamic-Validation/app.js
@@ -1,3 +1,21 @@
 function validate() {
-    // TODO
+
+    let emailInputElement = document.querySelector('#email');
+    let emailValidator = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g
+
+    function isValidEmail(str) {
+        if (emailValidator.test(str)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    function applyStyle(e, email) {
+        e.className = isValidEmail(email) ? '' : 'error';
+    }
+
+    emailInputElement.addEventListener('change', (e) => {
+        applyStyle(e.target, e.target.value)
+    });
 }
